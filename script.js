6f1a586c7ef3d4236e046c0b1ff72254175a55e9
AURELIA-HONEY <poool19900@gmail.com>
2026-04-27 08:13:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 let products = {
-  sidr:{name:"سدر",price:120,img:"images/sidr.jpg",desc:"عسل فاخر"},
+  sidr:{name:"سدر",price:120,img:"images/sidr.jpg"},
   talh:{name:"طلح",price:90,img:"images/talh.jpg"},
   samar:{name:"سمر",price:100,img:"images/samar.jpg"}
 };
 
+/* عرض المنتجات */
 function renderProducts(){
   let box=document.getElementById("products");
   for(let id in products){
     let p=products[id];
     box.innerHTML+=`
-      <div class="card">
-        <img src="${p.img}">
-        <h4>${p.name}</h4>
-        <p>${p.price} ريال</p>
-        <a href="product.html?id=${id}">
-          <button>عرض</button>
-        </a>
-      </div>`;
+    <div class="card">
+      <img src="${p.img}">
+      <h4>${p.name}</h4>
+      <p>${p.price} ريال</p>
+      <a href="product.html?id=${id}">
+        <button class="primary">عرض</button>
+      </a>
+    </div>`;
   }
 }
 
+/* صفحة المنتج */
 function loadProduct(){
   let id=new URLSearchParams(location.search).get("id");
   let p=products[id];
@@ -30,6 +32,7 @@ function loadProduct(){
   document.getElementById("price").innerText=p.price+" ريال";
 }
 
+/* إضافة */
 function addProduct(){
   let size=parseFloat(document.getElementById("size").value);
   let cart=JSON.parse(localStorage.getItem("cart"))||[];
@@ -43,15 +46,18 @@ function addProduct(){
   });
 
   localStorage.setItem("cart",JSON.stringify(cart));
+  updateCartCount();
   showToast();
 }
 
+/* Toast */
 function showToast(){
   let t=document.getElementById("toast");
   t.classList.add("show");
   setTimeout(()=>t.classList.remove("show"),2000);
 }
 
+/* سلة */
 function openCart(){
   document.getElementById("drawer").classList.add("open");
   document.querySelector(".overlay").classList.add("show");
@@ -63,6 +69,7 @@ function closeCart(){
   document.querySelector(".overlay").classList.remove("show");
 }
 
+/* عرض السلة */
 function loadDrawer(){
   let cart=JSON.parse(localStorage.getItem("cart"))||[];
   let box=document.getElementById("drawerItems");
@@ -73,35 +80,45 @@ function loadDrawer(){
     total+=i.price*i.qty;
 
     box.innerHTML+=`
-      <div class="cart-item">
-        <img src="${i.img}">
-        <div>
-          ${i.name}
-          <br>${i.size}ك
-          <br>${i.price*i.qty} ريال
-          <br>
-          <button onclick="changeQty(${index},1)">+</button>
-          ${i.qty}
-          <button onclick="changeQty(${index},-1)">-</button>
-        </div>
-      </div>`;
+    <div class="cart-item">
+      <img src="${i.img}">
+      <div>
+        <b>${i.name}</b>
+        <br>${i.size}ك
+        <br>${i.price*i.qty} ريال
+        <br>
+        <button onclick="changeQty(${index},1)">+</button>
+        ${i.qty}
+        <button onclick="changeQty(${index},-1)">-</button>
+      </div>
+    </div>`;
   });
 
-  document.getElementById("drawerTotal").innerText="الإجمالي: "+total;
+  document.getElementById("drawerTotal").innerText="الإجمالي: "+total+" ريال";
 }
 
+/* تعديل الكمية */
 function changeQty(i,c){
   let cart=JSON.parse(localStorage.getItem("cart"))||[];
   cart[i].qty+=c;
   if(cart[i].qty<=0) cart.splice(i,1);
   localStorage.setItem("cart",JSON.stringify(cart));
   loadDrawer();
+  updateCartCount();
 }
 
+/* عداد */
+function updateCartCount(){
+  let cart=JSON.parse(localStorage.getItem("cart"))||[];
+  document.getElementById("cartCount").innerText=cart.length;
+}
+
+/* انتقال */
 function goCheckout(){
   window.location.href="checkout.html";
 }
 
+/* Checkout */
 function loadCheckout(){
   let cart=JSON.parse(localStorage.getItem("cart"))||[];
   let box=document.getElementById("summary");
@@ -112,7 +129,7 @@ function loadCheckout(){
     box.innerHTML+=`<div>${i.name} × ${i.qty}</div>`;
   });
 
-  document.getElementById("total").innerText=total+" ريال";
+  document.getElementById("total").innerText="الإجمالي: "+total+" ريال";
 }
 
 function confirmOrder(){
